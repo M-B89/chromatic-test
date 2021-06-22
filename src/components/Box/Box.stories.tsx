ee968915ee4146598efaca1a2d880d0213097aa3
Mirko Bartkowiak <mirko.bartkowiak@materna.de>
2021-06-22 16:38:24 +0200

    add story for box

---
+++ src/components/Box/Box.stories.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import {Meta, Story} from '@storybook/react';
+import Box from '../Box/Box';
+
+export default {
+    title: 'Box',
+    component: Box,
+} as Meta;
+
+export const Basic: Story = () =>
+        <div className="App" style={{position: "relative"}}>
+            <Box>Box 1</Box>
+        </div>;
+
