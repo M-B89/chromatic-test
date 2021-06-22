cbbca4cad49fc77cc28d08b98f2a5d362b921c5c
mirko <mirko.bartkowiak@materna.de>
2021-06-22 16:05:22 +0200

    commit example impl

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,28 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Box from './components/Box/Box';
+import Grid from './components/Grid/Grid';
+import GridRuler from './components/GridRuler/GridRuler';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+            <div className="App" style={{position: "relative"}}>
+                <GridRuler spacing="md"> </GridRuler>
+                <Grid container spacing="md" justifyContent="center" alignItems="flex-end">
+                    <Grid item xs={2} sm={6} md={4} lg={3}>
+                        <Box>Box 1</Box>
+                    </Grid>
+                    <Grid item xs={2} sm={6} md={4} lg={3}>
+                        <Box>Box 2</Box>
+                    </Grid>
+                    <Grid item xs={2} sm={6} md={4} lg={3}>
+                        <Box>Box 3</Box>
+                    </Grid>
+                    <Grid item xs={2} sm={6} md={4} lg={3}>
+                        <Box>Box 3</Box>
+                    </Grid>
+                </Grid>
+            </div>
+    );
 }
 
 export default App;
---
+++ src/components/Grid/Grid.stories.tsx
@@ -0,0 +1,68 @@
+import React from 'react';
+import {Meta, Story} from '@storybook/react';
+import Grid from './Grid';
+import GridRuler from '../GridRuler/GridRuler';
+import Box from '../Box/Box';
+
+export default {
+    title: 'Grid',
+    component: Grid,
+} as Meta;
+
+export const Basic: Story = () =>
+        <div className="App" style={{position: "relative"}}>
+            <Grid container spacing="md">
+                <Grid item xs={12} sm={6} md={4} lg={3}>
+                    <Box>Box 1</Box>
+                </Grid>
+                <Grid item xs={12} sm={6} md={4} lg={3}>
+                    <Box>Box 2</Box>
+                </Grid>
+                <Grid item xs={12} sm={6} md={4} lg={3}>
+                    <Box>Box 3</Box>
+                </Grid>
+                <Grid item xs={12} sm={6} md={4} lg={3}>
+                    <Box>Box 3</Box>
+                </Grid>
+            </Grid>
+        </div>;
+
+
+export const WithRuler: Story = () =>
+        <div className="App" style={{position: "relative"}}>
+            <GridRuler spacing="md"> </GridRuler>
+            <Grid container spacing="md">
+                <Grid item xs={12} sm={6} md={4} lg={3}>
+                    <Box>Box 1</Box>
+                </Grid>
+                <Grid item xs={12} sm={6} md={4} lg={3}>
+                    <Box>Box 2</Box>
+                </Grid>
+                <Grid item xs={12} sm={6} md={4} lg={3}>
+                    <Box>Box 3</Box>
+                </Grid>
+                <Grid item xs={12} sm={6} md={4} lg={3}>
+                    <Box>Box 3</Box>
+                </Grid>
+            </Grid>
+        </div>;
+
+export const Interactive: Story = (args) =>
+        <div className="App" style={{position: "relative", height: '100vh'}}>
+            <GridRuler spacing="md"> </GridRuler>
+            <Grid container spacing="md" style={{height: '100%'}} {...args}>
+                <Grid item xs={1}>
+                    <Box>Box 1</Box>
+                </Grid>
+                <Grid item xs={1}>
+                    <Box>Box 2</Box>
+                </Grid>
+                <Grid item xs={1}>
+                    <Box>Box 3</Box>
+                </Grid>
+                <Grid item xs={1}>
+                    <Box>Box 3</Box>
+                </Grid>
+            </Grid>
+        </div>;
+
