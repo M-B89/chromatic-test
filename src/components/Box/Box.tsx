a56f16d6e28bf01b62dc0f40effaeeca7bdd1fb1
mirko <mirko.bartkowiak@materna.de>
2021-06-23 13:57:41 +0200

    test to get changes

--- src/components/Box/Box.tsx
+++ src/components/Box/Box.tsx
@@ -2,7 +2,10 @@ import React, {FunctionComponent, ReactNode} from 'react';
 import styles from './Box.module.scss'
 
 const Box: FunctionComponent<{ children: ReactNode }> = ({children}) => {
-    return <div className={styles.Box}>{children}</div>
+    return <div className={styles.Box}>
+        <h1>Headline of Box</h1>
+        {children}
+    </div>
 }
 
 export default Box;
