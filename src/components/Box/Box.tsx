eb85ba353b5034fa7feca7121dc643c8d1607081
mirko <mirko.bartkowiak@materna.de>
2021-06-23 14:14:19 +0200

    revert change

--- src/components/Box/Box.tsx
+++ src/components/Box/Box.tsx
@@ -3,7 +3,6 @@ import styles from './Box.module.scss'
 
 const Box: FunctionComponent<{ children: ReactNode }> = ({children}) => {
     return <div className={styles.Box}>
-        <h1>Headline of Box</h1>
         {children}
     </div>
 }
